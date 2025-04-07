6fcb56eb4c43c36af39c6da8eeb13b0181347bd0
Lena Heisel <lenaheisel@yahoo.de>
2025-04-07 21:25:43 +0200

    can build an executable .dll file Add .gitignore

---
+++ first-nx-open-test-project/first-nx-open-test-project/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstNxOpenTestProject
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //NXOpen.Session.GetSession().ListingWindow.Open();
+            //NXOpen.Session.GetSession().ListingWindow.WriteLine("Hello World!");
+
+            ShowCAMOperations dialog = new ShowCAMOperations();
+            dialog.Launch();
+        }
+    }
+}
